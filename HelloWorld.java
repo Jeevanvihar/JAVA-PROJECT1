a2522358d93011e0c16ef946b894dca994108ba1
sravan513 <sravan.06513@gmail.com>
2024-11-30 16:51:04 +0530

    hello file

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+class HelloWorld {
+// your program begins with a call to main ().
+// prints "Hello World" to the terminal window.
+public static void main(String[] args) {
+    System.out.println("Hello, World");
+}
+}
