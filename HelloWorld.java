ccb2536968bb61ed5a3b3a3213770eb7bda22f6b
Jeevanvihar <lahariavula7674@gmail.com>
2024-11-30 17:00:34 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ class HelloWorld {
 // prints "Hello World" to the terminal window.
 public static void main(String[] args) {
     System.out.println("Hello, World");
-}
+ System.out.println("Hello, World 2");
+}  
 }
